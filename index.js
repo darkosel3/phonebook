04f40e966342dc742f8d029bea12aa9cb650a9c6
darkosel3 <darkosel7@gmail.com>
2024-12-04 13:49:33 +0100

    uibuild

--- index.js
+++ index.js
@@ -2,45 +2,34 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models/person.js')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = Person
 app.get('/api/persons',(request,response)=>{
-    response.json(persons)
+    Person.find({}).then(persons =>{
+        console.log(persons.length)
+        response.json(persons)
+    })
 })
 app.get('/info',(request,response)=>{
     const now = new Date();
-    response.send(`<p>Phonebook has info for ${persons.length} people <br/> ${now} </p>`)
+    let len
+    Person.find({}).then(persons => { len = persons.length})
+    response.send(`<p>Phonebook has info for ${len} people <br/> ${now} </p>`)
 })
-app.get('/api/persons/:id',(request,response)=>{
+
+app.get('/api/persons/:id',(request,response,next)=>{
     const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    response.json(person)
+    Person.findById(id).then(person => {
+        response.json(person)
+    }).catch(err => next(err))
+    
 })
 
 const PORT = process.env.PORT || 3001
@@ -48,34 +37,51 @@ app.listen(PORT,() => {
     console.log("yo we are listening at port "+PORT)
 })
 
-app.delete('/api/persons/:id',(request,response)=>{
-    const id = request.params.id
-    persons = persons.filter(person => person.id != id)
+app.delete('/api/persons/:id',(request,response,next)=>{
+    const id = new mongoose.Types.ObjectId(request.params.id);
+    console.log(typeof id)
+    Person.findByIdAndDelete(id).then(res => console.log(res)).catch(err => next(err))
+    
+    // persons = persons.filter(person => person.id != id)
     response.status(204).end()
 })
 
-app.post('/api/persons',(request,response)=>{ // proveri da li radi kada se vratis
-    let id = Math.floor(1+ Math.random()* 1000000).toString()
+app.post('/api/persons', async (request,response)=>{ 
+    const {name, number} = request.body
+
     if (!request.body.name || !request.body.number){
-        return response.status(400).json({
-            error: 'name or number is missing'
-        })
-    }else if(persons.some(person => person.name == request.body.name)){
-        return response.status(400).json({
-            error: `${request.body.name} already exists`
-        })
+        return response.status(400).json({error: 'name or number is missing'})
     }
-    let person = {
-        id: id,
-        name: request.body.name,
-        numer: request.body.number
+    
+    try {
+        const existingPerson = await Person.findOne({name})
+        if (existingPerson) {
+            return response.status(400).json({error: `${name} already exists`,});
+        }
+        let person = new Person({name,number}).save().then(person => response.json(person))
+    }catch (error) {
+        console.error('Error occurred:', error);
+        response.status(500).json({ error: 'Internal server error' });
     }
-    persons = persons.concat(person)
-    response.json(person)
 })
+
+app.put('/api/persons/:id', (request,response) => {
+    // console.log( request.body)
+    const _id = new mongoose.Types.ObjectId(request.params.id)
+    Person.findByIdAndUpdate(_id,request.body).then(person => response.status(200).send(person))
+})
+
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
+  const errorHandler = (error,request,response,next) =>{
 
+        if(error.name === 'CastError'){
+            return response.status(400).send({error:'malformated id'})
+        }
+
+        next(error) // delegates to the next middleware
+  }
   app.use(unknownEndpoint)
+  app.use(errorHandler)
---
+++ models/person.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+// def the url,type of query and connection
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+
+console.log('connecting to',url)
+
+mongoose.connect(url).then(res =>{
+    console.log("Succesful connection")
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
+});
+
+// define the schema for db
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+// create a model for backend
+// const Person = mongoose.model('Person',personSchema);
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+
+
+  module.exports = mongoose.model('Person', personSchema)
+
+
+
+
+// if(numArgs == 5){
+// const person = new Person({
+//     name: personName,
+//     number: personNumber
+// })
+// person.save().then(result =>{
+//     console.log(result.name + " is saved with number " + result.number)
+//     mongoose.connection.close()
+// })
+// }else if(numArgs == 3){
+// Person.find({}).then(result =>{
+//     result.forEach(person =>{
+//         console.log(person)
+//     })
+//     mongoose.connection.close()
+// })
+// }
